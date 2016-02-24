f7f8d42c8a193efe2da9a198674bc22b3881456a
lentusignavus <ignavus.ii@gmail.com>
2016-02-23 21:54:46 -0600

    WIP: Implementing widget

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/Widget.java
@@ -0,0 +1,16 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by kare2436 on 2/23/16.
+ */
+public class Widget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
