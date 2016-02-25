3e1f0a01a224ee99181392cde96e7af8af9f2428
lentusignavus <ignavus.ii@gmail.com>
2016-02-24 22:27:21 -0600

    WIP: Making significant process on Widget integration

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/Widget.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/Widget.java
@@ -3,14 +3,35 @@ package barqsoft.footballscores;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
-/**
- * Created by kare2436 on 2/23/16.
- */
+
 public class Widget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Toast.makeText(context, "Widget Created", Toast.LENGTH_SHORT).show();
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
     }
 }
